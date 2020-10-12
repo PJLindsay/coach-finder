a5956a27a2c071ce43dcab6866dd4460760a6407
PJ Lindsay <pjlindsay@gmail.com>
2020-10-12 16:35:10 -0600

    Add Coach Registration

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,21 @@
 <template>
-  REGISTER AS A COACH
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue'
+export default {
+
+  components: {
+    CoachForm
+  }
+
+}
+</script>
+
+
