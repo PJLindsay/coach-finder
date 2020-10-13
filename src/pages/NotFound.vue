5384027d630cf880126933d38bc0bd4854604fbb
PJ Lindsay <pjlindsay@gmail.com>
2020-10-12 20:59:21 -0600

    Add 'Not Found' page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,10 @@
 <template>
-  NOT FOUND
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>Page was not found - perhaps check out all our
+        <router-link to="/coaches">Coaches</router-link>
+      </p>
+    </base-card>
+  </section>
 </template>
