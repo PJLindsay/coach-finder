9f677bc6daedd5bf5596097c510a549ad73ce75b
PJ Lindsay <pjlindsay@gmail.com>
2020-10-21 20:58:24 -0600

    Fix missing coach.id from coach list

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,8 +1,8 @@
-import {fbkey as firebaseurl} from '../../../firebase.js'
+import { fbkey as firebaseurl } from '../../../firebase.js';
 
 export default {
   async registerCoach(context, data) {
-    const userId = context.rootGetters.userId
+    const userId = context.rootGetters.userId;
 
     const coachData = {
       firstName: data.first,
@@ -10,7 +10,7 @@ export default {
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas
-    }
+    };
 
     // access a protected resource by getting our token
     // const token = context.rootGetters.token
@@ -19,51 +19,50 @@ export default {
     const response = await fetch(`${firebaseurl}/coaches/${userId}.json`, {
       method: 'PUT',
       body: JSON.stringify(coachData)
-    })
+    });
 
-    const resData = await response.json()
+    const resData = await response.json();
 
     if (!response.ok) {
-      const error = new Error(resData.message || 'Failed to fetch')
-      throw error
+      const error = new Error(resData.message || 'Failed to fetch');
+      throw error;
     }
 
     // copy coachData and merge with userId for local mutation
     context.commit('registerCoach', {
       ...coachData,
       id: userId
-    })
+    });
   },
 
   async loadCoaches(context, payload) {
-
     if (!payload.forceRefresh && !context.getters.shouldUpdate) {
-      return // don't get data again (wait until coach list is >1 minute old) or if user clicked 'refresh' button
+      return; // wait until coach list is >1 minute old *or* user clicks 'refresh' button
     }
 
-    const response = await fetch(`${firebaseurl}/coaches.json`)
-    const resData = await response.json()
+    const response = await fetch(`${firebaseurl}/coaches.json`);
+    const resData = await response.json();
 
     if (!response.ok) {
-      const error = new Error(resData.message || 'Failed to fetch')
-      throw error
+      const error = new Error(resData.message || 'Failed to fetch');
+      throw error;
     }
 
-    const coaches = []
+    const coaches = [];
 
     for (const key in resData) {
       const coach = {
+        id: key,
         firstName: resData[key].firstName,
         lastName: resData[key].lastName,
         description: resData[key].description,
         hourlyRate: resData[key].hourlyRate,
         areas: resData[key].areas
-      }
-      coaches.push(coach)
+      };
+      coaches.push(coach);
     }
 
-    context.commit('setCoaches', coaches)
-    context.commit('setFetchTimestamp') // flag this so we know when data was last fetched
+    context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp'); // flag this so we know when data was last fetched
   }
-
-}
+};
